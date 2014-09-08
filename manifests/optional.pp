2a1ec6cda5caeb466fe81909e9fff25cad59b4cf
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-09-08 16:28:53 +0100

    Ensured redis is installed

--- manifests/optional.pp
+++ manifests/optional.pp
@@ -1,6 +1,8 @@
 class devkit::optional inherits devkit::params {
   include devkit::optional::postgres
   include devkit::optional::elasticsearch
+  include devkit::optional::redis
+
   # RabbitMQ 3.1.5
   package { $rabbitmq :
     ensure   => latest

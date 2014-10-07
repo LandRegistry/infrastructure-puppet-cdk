524d3eb2526731455c074016c372f282c614cc4b
Ben Fairless <ben.fairless@landregistry.gsi.gov.uk>
2014-10-07 10:57:14 +0100

    Fixed dependency on stdlib

--- manifests/optional/elasticsearch.pp
+++ manifests/optional/elasticsearch.pp
@@ -1,21 +1,22 @@
 class devkit::optional::elasticsearch {
   # Elasticsearch 1.3
-  file_line  { 'debian_package':
-    path => '/etc/apt/sources.list',
-    line => 'deb http://packages.elasticsearch.org/elasticsearch/1.3/debian stable main',
+  file  { 'elasticsearch apt repo':
+    mode    => '0644',
+    path    => '/etc/apt/sources.list.d/elasticsearch.list',
+    content => 'deb http://packages.elasticsearch.org/elasticsearch/1.3/debian stable main',
     require => Exec['elasticsearch key']
   }
-  exec { "elasticsearch key":
+  exec { 'elasticsearch key':
     command => "wget -qO - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | apt-key add -",
-    path => [ '/bin', '/usr/bin', '/usr/sbin' ]
+    path    => [ '/bin', '/usr/bin', '/usr/sbin' ]
   }
-  exec { "apt-get update":
+  exec { 'apt-get update':
     command => "/usr/bin/apt-get update",
-    onlyif => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
-    require => File_line['debian_package']
+    onlyif  => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
+    require => File['elasticsearch apt repo']
   }
-  package { "elasticsearch":
-    ensure => latest,
+  package { 'elasticsearch':
+    ensure  => latest,
     require => Exec['apt-get update']
   }
 }
